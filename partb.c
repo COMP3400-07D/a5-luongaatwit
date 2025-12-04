54b7f25c3c69559b6384e1ae746e45ddf68dcb6f
luongaatwit <luonga@wit.edu>
2025-12-03 23:59:26 -0500

    completed part b

--- partb.c
+++ partb.c
@@ -6,6 +6,30 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
-    return 0;
+ if (argc != 3) {
+    printf("USAGE: partb FILEIN FILEOUT");
+    return 1;
+}
+char *filein = argv[1];
+char *fileout = argv[2];
+
+int fd = open(fileout, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+
+if (fd < 0) {
+    printf("ERROR: cannot open %s\n", fileout);
+    return 1;
+}
+
+dup2(fd,STDOUT_FILENO);
+close(fd);
+
+printf("Category,Count\n");
+fflush(stdout);
+
+char *args[] = {"./parta", filein, NULL};
+
+execv("./parta",args);
+
+printf("ERROR: exec failed\n");
+return 1;
 }
